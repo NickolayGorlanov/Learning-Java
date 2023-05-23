1a0ae981c2ad10f9a2eae104af071c34e3fd4cbd
nikolai_gorlanov <gorlanov.nickolay@yandex.ru>
2023-05-23 21:40:49 +0300

    edited

--- ObjectsAndClasses_StaticFinal/src/Computer.java
+++ ObjectsAndClasses_StaticFinal/src/Computer.java
@@ -1,67 +1,242 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Computer {
-    private final String vendor;
-    private final String name;
-    private Processor processor;
-    private final List<Ram> ramList;
-    private final List<HardDisc> hardDiscsList;
-    private Screen screen;
-    private Keyboard keyboard;
+    private final Processor processor;
+    private final Memory memory;
+    private final Storage storage;
+    private final Screen screen;
+    private final Keyboard keyboard;
 
-    public Computer(String vendor, String name) {
-        this.vendor = vendor;
-        this.name = name;
-        this.ramList = new ArrayList<>();
-        this.hardDiscsList = new ArrayList<>();
+    public enum ScreenType {
+        IPS(15.6, 0.5),
+        TN(17.3, 0.8),
+        OLED(17.3, 0.8);
+
+        private final double diagonal;
+        private final double weight;
+
+        ScreenType(double diagonal, double weight) {
+            this.diagonal = diagonal;
+            this.weight = weight;
+        }
+
+        public double getDiagonal() {
+            return diagonal;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
     }
 
-    public void setProcessor(Processor processor) {
+    public enum KeyboardType {
+        MECHANICAL("Mechanical", true, 0.7),
+        MEMBRANE("Membrane", false, 0.6);
+
+        private final String type;
+        private final boolean backlight;
+        private final double weight;
+
+        KeyboardType(String type, boolean backlight, double weight) {
+            this.type = type;
+            this.backlight = backlight;
+            this.weight = weight;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public boolean hasBacklight() {
+            return backlight;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
+    }
+
+    public enum MemoryType {
+        DDR2(0.3, 4),
+        DDR3(0.4, 8),
+        DDR4(0.5, 16);
+
+        private final double weight;
+        private final int capacity;
+
+        MemoryType(double weight, int capacity) {
+            this.weight = weight;
+            this.capacity = capacity;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+    }
+
+    public enum StorageType {
+        HDD(0.7, 1000),
+        SSD(0.5, 500),
+        NVMe(0.4, 2000);
+
+        private final double weight;
+        private final int capacity;
+
+        StorageType(double weight, int capacity) {
+            this.weight = weight;
+            this.capacity = capacity;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+    }
+
+    public enum ProcessorType {
+        INTEL("Intel", 3.2, 4, 0.8),
+        AMD("AMD", 2.8, 6, 0.9),
+        ARM("ARM", 2.0, 2, 0.5);
+
+        private final String type;
+        private final double frequency;
+        private final int cores;
+        private final double weight;
+
+        ProcessorType(String type, double frequency, int cores, double weight) {
+            this.type = type;
+            this.frequency = frequency;
+            this.cores = cores;
+            this.weight = weight;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public double getFrequency() {
+            return frequency;
+        }
+
+        public int getCores() {
+            return cores;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
+
+        public Processor toProcessor() {
+            return new Processor(type, frequency, cores, weight);
+        }
+    }
+
+    public static class Processor {
+        private final String type;
+        private final double frequency;
+        private final int cores;
+        private final double weight;
+
+        private Processor(String type, double frequency, int cores, double weight) {
+            this.type = type;
+            this.frequency = frequency;
+            this.cores = cores;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return "Type: " + type + ", Frequency: " + frequency + " GHz, Cores: " + cores + ", Weight: " + weight + " kg";
+        }
+    }
+
+    public static class Memory {
+        private final String type;
+        private final int capacity;
+        private final double weight;
+
+        private Memory(String type, int capacity, double weight) {
+            this.type = type;
+            this.capacity = capacity;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return "Type: " + type + ", Capacity: " + capacity + " GB, Weight: " + weight + " kg";
+        }
+    }
+
+    public static class Storage {
+        private final String type;
+        private final int capacity;
+        private final double weight;
+
+        private Storage(String type, int capacity, double weight) {
+            this.type = type;
+            this.capacity = capacity;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return "Type: " + type + ", Capacity: " + capacity + " GB, Weight: " + weight + " kg";
+        }
+    }
+
+    public static class Screen {
+        private final double diagonal;
+        private final String type;
+        private final double weight;
+
+        public Screen(double diagonal, String type, double weight) {
+            this.diagonal = diagonal;
+            this.type = type;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return "Diagonal: " + diagonal + " inches, Type: " + type + ", Weight: " + weight + " kg";
+        }
+    }
+
+    public static class Keyboard {
+        private final String type;
+        private final boolean hasBacklight;
+        private final double weight;
+
+        public Keyboard(String type, boolean hasBacklight, double weight) {
+            this.type = type;
+            this.hasBacklight = hasBacklight;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return "Type: " + type + ", Backlight: " + (hasBacklight ? "Yes" : "No") + ", Weight: " + weight + " kg";
+        }
+    }
+
+    public Computer(Processor processor, MemoryType memoryType, StorageType storageType, ScreenType screenType, KeyboardType keyboardType) {
         this.processor = processor;
-    }
-
-    public void addRam(Ram ram) {
-        ramList.add(ram);
-    }
-
-    public void addHardDisc(HardDisc hardDisc) {
-        hardDiscsList.add(hardDisc);
-    }
-
-    public void setScreen(Screen screen) {
-        this.screen = screen;
-    }
-
-    public void setKeyboard(Keyboard keyboard) {
-        this.keyboard = keyboard;
-    }
-
-    public double getTotalWeight() {
-        double totalWeight = 0.0;
-        for (Ram ram : ramList) {
-            totalWeight += ram.getWeight();
-        }
-        for (HardDisc hardDisc : hardDiscsList) {
-            totalWeight += hardDisc.getWeight();
-        }
-        totalWeight += screen.getWeight();
-        totalWeight += keyboard.getWeight();
-        return totalWeight;
+        this.memory = new Memory(memoryType.toString(), memoryType.getCapacity(), memoryType.getWeight());
+        this.storage = new Storage(storageType.toString(), storageType.getCapacity(), storageType.getWeight());
+        this.screen = new Screen(screenType.getDiagonal(), screenType.name(), screenType.getWeight());
+        this.keyboard = new Keyboard(keyboardType.getType(), keyboardType.hasBacklight(), keyboardType.getWeight());
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Computer{\n");
-        sb.append("  vendor='").append(vendor).append("'\n");
-        sb.append("  name='").append(name).append("'\n");
-        sb.append("  processor=").append(processor).append("\n");
-        sb.append("  ramList=").append(ramList).append("\n");
-        sb.append("  hardDiscsList=").append(hardDiscsList).append("\n");
-        sb.append("  screen=").append(screen).append("\n");
-        sb.append("  keyboard=").append(keyboard).append("\n");
-        sb.append("}\n");
-        return sb.toString();
+        return "Processor: " + processor.toString() + "\n" +
+                "Memory: " + memory.toString() + "\n" +
+                "Storage: " + storage.toString() + "\n" +
+                "Screen: " + screen.toString() + "\n" +
+                "Keyboard: " + keyboard.toString();
     }
 }
--- ObjectsAndClasses_StaticFinal/src/Main.java
+++ ObjectsAndClasses_StaticFinal/src/Main.java
@@ -1,46 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        Processor processor1 = new Processor(2.4, 2, "ProcessorModel", 0.5);
-        Processor processor2 = new Processor(2.8, 2, "ProcessorModel2", 0.6);
 
-        Ram ram1 = new Ram("RamModel1", 8, 0.2);
-        Ram ram2 = new Ram("RamModel2", 16, 0.4);
-        Ram ram3 = new Ram("RamModel3", 4, 0.3);
-        Ram ram4 = new Ram("RamModel4", 8, 0.6);
 
-        HardDisc hardDisc1 = new HardDisc("HardDiscModel1", 500, 0.8);
-        HardDisc hardDisc2 = new HardDisc("HardDiscModel2", 1000, 1.2);
-        HardDisc hardDisc3 = new HardDisc("HardDiscModel3", 250, 0.5);
-        HardDisc hardDisc4 = new HardDisc("HardDiscModel4", 500, 1.0);
+        // Создание компонентов клавиатур
+        Computer.KeyboardType keyboard1 = Computer.KeyboardType.MECHANICAL;
+        Computer.KeyboardType keyboard2 = Computer.KeyboardType.MEMBRANE;
 
-        Screen screen1 = new Screen("ScreenModel", 15.6, 1.0);
-        Screen screen2 = new Screen("ScreenModel2", 13.3, 0.8);
+        // Создание типов экранов
+        Computer.ScreenType screen1 = Computer.ScreenType.IPS;
+        Computer.ScreenType screen2 = Computer.ScreenType.TN;
+        Computer.ScreenType screen3 = Computer.ScreenType.OLED;
 
-        Keyboard keyboard1 = new Keyboard("KeyboardModel", 0.3);
-        Keyboard keyboard2 = new Keyboard("KeyboardModel2", 0.4, true);
+        // Создание типов процессоров
+        Computer.ProcessorType processor1 = Computer.ProcessorType.AMD;
+        Computer.ProcessorType processor2 = Computer.ProcessorType.INTEL;
+        Computer.ProcessorType processor3 = Computer.ProcessorType.ARM;
 
-        Computer computer1 = new Computer("Vendor1", "ComputerName1");
-        computer1.setProcessor(processor1);
-        computer1.addRam(ram1);
-        computer1.addRam(ram2);
-        computer1.addHardDisc(hardDisc1);
-        computer1.addHardDisc(hardDisc2);
-        computer1.setScreen(screen1);
-        computer1.setKeyboard(keyboard1);
+        // Создание типов памяти
+        Computer.MemoryType memory1 = Computer.MemoryType.DDR4;
+        Computer.MemoryType memory2 = Computer.MemoryType.DDR3;
+        Computer.MemoryType memory3 = Computer.MemoryType.DDR2;
 
-        Computer computer2 = new Computer("Vendor2", "ComputerName2");
-        computer2.setProcessor(processor2);
-        computer2.addRam(ram3);
-        computer2.addRam(ram4);
-        computer2.addHardDisc(hardDisc3);
-        computer2.addHardDisc(hardDisc4);
-        computer2.setScreen(screen2);
-        computer2.setKeyboard(keyboard2);
+        // Создание типов хранилищ
+        Computer.StorageType storage1 = Computer.StorageType.NVMe;
+        Computer.StorageType storage2 = Computer.StorageType.SSD;
+        Computer.StorageType storage3 = Computer.StorageType.HDD;
 
-        System.out.println(computer1);
-        System.out.println("\nTotal weight: " + computer1.getTotalWeight());
+        // Создание объектов компьютеров
+        Computer computer1 = new Computer(processor1.toProcessor(), memory1, storage1, screen1, keyboard1);
+        Computer computer2 = new Computer(processor2.toProcessor(), memory2, storage2, screen2, keyboard2);
+        Computer computer3 = new Computer(processor3.toProcessor(), memory3, storage3, screen3, keyboard1);
 
-        System.out.println(computer2);
-        System.out.println("\nTotal weight: " + computer2.getTotalWeight());
+
+
+
+        // Вывод спецификаций компьютеров
+        System.out.println();
+        System.out.println(computer1.toString());
+        System.out.println();
+        System.out.println(computer2.toString());
+        System.out.println();
+        System.out.println(computer3.toString());
+
     }
+
 }
