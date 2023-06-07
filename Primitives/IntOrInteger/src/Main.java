56ffdc8c045a62ece134e32b8c9ad0ffae83e4bc
nikolai_gorlanov <gorlanov.nickolay@yandex.ru>
2023-06-07 15:13:42 +0300

    С помощью цикла добавлены все коды букв русского алфавита - заглавных и строчных, в том числе буквы Ё

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -1,18 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
-        container.addCount(5672);
+        container.addCount(33); // Ограничиться только алфавитом русского языка
         System.out.println(container.getCount());
 
-        // TODO: ниже напишите код для выполнения задания:
-        //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
-
-        for (int i = 0; i < 5672; i++) {
+        // Найти все числовые коды символов алфавита русского языка
+        for (int i = 1072; i <= 1103; i++) {
             char c = (char) i;
             System.out.println(i + " - " + c);
-
         }
+
+        for (int i = 1040; i <= 1071; i++) {
+            char c = (char) i;
+            System.out.println(i + " - " + c);
+        }
+
+        char lowercaseYo = 'ё'; // Символ строчной буквы ё
+        char uppercaseYo = 'Ё'; // Символ прописной буквы Ё
+
+        System.out.println((int) lowercaseYo + " - " + lowercaseYo);
+        System.out.println((int) uppercaseYo + " - " + uppercaseYo);
+
     }
 
 }
+
