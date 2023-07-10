1bcf4b322e04a1445cdd5fb57321803ded584a9d
gorlanov_nikolai <gorlanov.nickolay@yandex.ru>
2023-07-10 14:31:01 +0300

    modified

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
@@ -9,27 +12,50 @@ public class Main {
             COMMAND_EXAMPLES;
     private static final String HELP_TEXT = "Command examples:\n" + COMMAND_EXAMPLES;
 
+    public static final Logger LOGGER = LogManager.getLogger(Main.class);
+    public static final Logger ERRORS_LOGGER = LogManager.getLogger("ErrorsLogger");
+    public static final Logger QUERIES_LOGGER = LogManager.getLogger("QueriesLogger");
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
 
-        while (true) {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
+        LOGGER.info("Application started.");
 
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(HELP_TEXT);
-            } else {
-                System.out.println(COMMAND_ERROR);
+        while (true) {
+            try {
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
+
+                QUERIES_LOGGER.info("Command: {}", command);
+
+                if (tokens[0].equals("add")) {
+                    try {
+                        executor.addCustomer(tokens[1]);
+                    } catch (CustomerStorage.InvalidDataFormatException | CustomerStorage.InvalidPhoneNumberException | CustomerStorage.InvalidEmailException e) {
+                        System.err.println("An error occurred: " + e.getMessage());
+                        ERRORS_LOGGER.error("Error adding customer: {}", e.getMessage());
+                    }
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(HELP_TEXT);
+                } else if (tokens[0].equals("exit")) {
+                    break; // Выход из цикла при вводе команды "exit"
+                } else {
+                    System.out.println(COMMAND_ERROR);
+                    ERRORS_LOGGER.error("Invalid command: {}", command);
+                }
+            } catch (RuntimeException e) {
+                System.err.println("An error occurred: " + e.getMessage());
+                ERRORS_LOGGER.error("Error executing command: {}", e.getMessage());
             }
         }
+
+        LOGGER.info("Application stopped.");
     }
 }
