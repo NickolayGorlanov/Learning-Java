46ccc110aae84b554f67d67e0a05b5d3aa629bd6
gorlanov_nikolai <gorlanov.nickolay@yandex.ru>
2023-08-05 06:55:52 +0300

    modified

--- FilesAndNetwork/DataCollector/src/main/java/DataCollector/Main.java
+++ FilesAndNetwork/DataCollector/src/main/java/DataCollector/Main.java
@@ -1,6 +1,7 @@
 package DataCollector;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,92 +14,107 @@ public class Main {
         String url = "https://skillbox-java.github.io/";
         WebPageParser parser = new WebPageParser(url);
 
+        String html;
+        List<SubwayLine> subwayLines = new ArrayList<>();
+        List<SubwayStation> subwayStations = new ArrayList<>();
+
         try {
-            String html = parser.getHTML();
-            List<SubwayLine> subwayLines = parser.parseSubwayLines(html);
-            List<SubwayStation> subwayStations = parser.parseSubwayStations(html);
+            html = parser.getHTML();
+            subwayLines = parser.parseSubwayLines(html);
+            subwayStations = parser.parseSubwayStations(html);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            Gson gson = new Gson();
+        Gson gson = new Gson();
 
+        try (FileWriter linesFileWriter = new FileWriter("subway_lines.json")) {
+            gson.toJson(subwayLines, linesFileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            try (FileWriter linesFileWriter = new FileWriter("subway_lines.json")) {
-                gson.toJson(subwayLines, linesFileWriter);
-            }
+        try (FileWriter stationsFileWriter = new FileWriter("subway_stations.json")) {
+            gson.toJson(subwayStations, stationsFileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            try (FileWriter stationsFileWriter = new FileWriter("subway_stations.json")) {
-                gson.toJson(subwayStations, stationsFileWriter);
-            }
+        System.out.println("Данные успешно разобраны и сохранены в JSON-файлах.");
 
-            System.out.println("Данные успешно разобраны и сохранены в JSON-файлах.");
+        String folderPath = "/home/user/IdeaProjects/DataCollector/stations-data/data";
+        FileSearcher fileSearcher = new FileSearcher();
 
-            String folderPath = "/home/user/IdeaProjects/DataCollector/stations-data/data";
-            FileSearcher fileSearcher = new FileSearcher();
+        try {
+            fileSearcher.searchFiles(folderPath);
+            List<String> jsonFiles = fileSearcher.getJsonFiles();
+            List<String> csvFiles = fileSearcher.getCsvFiles();
 
-            try {
-                fileSearcher.searchFiles(folderPath);
-                List<String> jsonFiles = fileSearcher.getJsonFiles();
-                List<String> csvFiles = fileSearcher.getCsvFiles();
+            List<SubwayStationData> subwayStationDataList = new ArrayList<>();
 
-                List<SubwayStationData> subwayStationDataList = new ArrayList<>();
+            System.out.println("Найденные JSON-файлы:");
+            for (String jsonFile : jsonFiles) {
+                System.out.println(jsonFile);
 
-                System.out.println("Найденные JSON-файлы:");
-                for (String jsonFile : jsonFiles) {
-                    System.out.println(jsonFile);
-
-                    // Чтение JSON данных из файла
-                    String jsonData = new String(Files.readAllBytes(Paths.get(jsonFile)));
-                    List<SubwayStationData> stationDataList = JsonParser.parseJsonData(jsonData);
-
-                    if (stationDataList != null) {
-                        subwayStationDataList.addAll(stationDataList);
-
-                        // Вывод данных из разобранных JSON объектов
-                        for (SubwayStationData stationData : stationDataList) {
-                            System.out.println("Название станции: " + stationData.getStation_name());
-                            String depth = stationData.getDepth();
-                            System.out.println("Глубина: " + (depth != null ? depth : "Неизвестно"));
-                            System.out.println();
-                        }
-                    }
+                // Чтение JSON данных из файла
+                String jsonData;
+                try {
+                    jsonData = new String(Files.readAllBytes(Paths.get(jsonFile)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    continue;
                 }
 
-                System.out.println("\nНайденные CSV-файлы:");
-                for (String csvFile : csvFiles) {
-                    System.out.println(csvFile);
+                List<SubwayStationData> stationDataList = JsonParser.parseJsonData(jsonData);
+                if (stationDataList != null) {
+                    subwayStationDataList.addAll(stationDataList);
 
-                    // Парсинг CSV данных из файла
-                    List<SubwayStationDataCsv> stationDataList = CsvParser.parseCsvData(csvFile);
-                    subwayStationDataList.addAll(convertToSubwayStationData(stationDataList));
-
-                    // Вывод данных из разобранных CSV строк
-                    for (SubwayStationDataCsv stationData : stationDataList) {
-                        System.out.println("Название станции: " + stationData.getName());
-                        System.out.println("Дата открытия: " + stationData.getDate());
+                    // Вывод данных из разобранных JSON объектов
+                    for (SubwayStationData stationData : stationDataList) {
+                        System.out.println("Название станции: " + stationData.getStation_name());
+                        String depth = stationData.getDepth();
+                        System.out.println("Глубина: " + (depth != null ? depth : "Неизвестно"));
                         System.out.println();
                     }
                 }
+            }
 
-                // Создание списка станций по линиям
-                MetroData metroData = new MetroData(subwayStationDataList, subwayLines);
+            System.out.println("\nНайденные CSV-файлы:");
+            for (String csvFile : csvFiles) {
+                System.out.println(csvFile);
 
-                // Запись списка станций по линиям в файл map.json
-                try (FileWriter mapFileWriter = new FileWriter("map.json")) {
-                    gson.toJson(metroData, mapFileWriter);
+                // Парсинг CSV данных из файла
+                List<SubwayStationDataCsv> stationDataList = CsvParser.parseCsvData(csvFile);
+                subwayStationDataList.addAll(convertToSubwayStationData(stationDataList));
+
+                // Вывод данных из разобранных CSV строк
+                for (SubwayStationDataCsv stationData : stationDataList) {
+                    System.out.println("Название станции: " + stationData.getName());
+                    System.out.println("Дата открытия: " + stationData.getDate());
+                    System.out.println();
                 }
+            }
 
-                // Создание списка свойств станций
-                MetroData stationsData = new MetroData(subwayStationDataList, null);
+            // Создание списка станций по линиям
+            MetroData metroData = new MetroData(subwayStationDataList, subwayLines);
 
-                // Запись списка свойств станций в файл stations.json
-                try (FileWriter stationsDataFileWriter = new FileWriter("stations.json")) {
-                    gson.toJson(stationsData, stationsDataFileWriter);
-                }
-
-                System.out.println("\nВсего найдено JSON-файлов: " + jsonFiles.size());
-                System.out.println("Всего найдено CSV-файлов: " + csvFiles.size());
+            try (FileWriter mapFileWriter = new FileWriter("map.json")) {
+                gson.toJson(metroData, mapFileWriter);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            // Создание списка свойств станций
+            MetroData stationsData = new MetroData(subwayStationDataList, null);
+
+            try (FileWriter stationsDataFileWriter = new FileWriter("stations.json")) {
+                gson.toJson(stationsData, stationsDataFileWriter);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("\nВсего найдено JSON-файлов: " + jsonFiles.size());
+            System.out.println("Всего найдено CSV-файлов: " + csvFiles.size());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -114,8 +130,4 @@ public class Main {
         }
         return stationDataList;
     }
-
-
-
-       }
-
+}
