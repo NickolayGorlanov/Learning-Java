641bdb16bb6923ce8cd78b75c066046891a8b034
nickolai_gorlanov <gorlanov.nickolay@yandex.ru>
2023-09-07 13:15:08 +0300

    modified

--- SQLAndHibernate/Java_17_Hibernate_app/src/main/java/Hibernate_app/Teacher.java
+++ SQLAndHibernate/Java_17_Hibernate_app/src/main/java/Hibernate_app/Teacher.java
@@ -8,17 +8,17 @@ import java.util.List;
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     private String name;
     private int salary;
     private int age;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
